6d12a6d40e4b43f3f6491b38ef922db1039f6c3f
swagere <1250539180@qq.com>
2021-01-28 16:26:25 +0800

    init

---
+++ src/test/java/com/kve/talent_management_system/TalentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kve.talent_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
